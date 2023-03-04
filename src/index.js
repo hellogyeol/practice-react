1747e7cb818e3ca5a9f8949fcf0abf1ec03d23a0
hellogyeol <hellogyeol@gmail.com>
2023-03-04 12:34:40 +0900

    test: 첫 렌더링

--- src/index.js
+++ src/index.js
@@ -2,4 +2,8 @@ import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(<h1>Hello, world!</h1>);
+root.render(
+  <>
+    <h1>React App</h1>
+  </>
+);
